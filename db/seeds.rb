d502754d99fe8ad17e555245b8d0fdcf8f8d9c18
kirabiyakasa <morning_star@kirabiyakasa.moe>
2021-12-26 11:22:00 -0500

    Add name column to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(name: "user1", email: "user_one@example.com", password: "abc123")
+user2 = User.create(name: "user2", email: "user_two@example.com", password: "abc123")
+user2 = User.create(name: "user3", email: "user_three@example.com", password: "abc123")
